d4a7d3bc58b22186b3804cbf80a262a72067ba68
dioptre <andrew@expedit.com.au>
2013-08-09 02:08:37 +0000

    protected dev area

--- src/main/Roster.Orchard/src/Orchard.Web/Modules/EXPEDIT.Roster/Permissions.cs
+++ src/main/Roster.Orchard/src/Orchard.Web/Modules/EXPEDIT.Roster/Permissions.cs
@@ -6,20 +6,22 @@ namespace EXPEDIT.Roster {
     public class Permissions : IPermissionProvider {
         public static readonly Permission CompanyOwner = new Permission { Description = "Company Owner", Name = "CompanyOwner" };
         public static readonly Permission CompanyManager = new Permission { Description = "Company Manager", Name = "CompanyManager", ImpliedBy = new[] { CompanyOwner } };
+        public static readonly Permission Developer = new Permission { Description = "Developer", Name = "Developer" };
 
         public virtual Feature Feature { get; set; }
 
         public IEnumerable<Permission> GetPermissions() {
             return new[] {
                 CompanyOwner,
-                CompanyManager
-            };
+                CompanyManager,
+                Developer
+            };            
         }
 
         public IEnumerable<PermissionStereotype> GetDefaultStereotypes() {
             return new[] {
                 new PermissionStereotype {
-                    Name = "Administrator",
+                    Name = "Administrator", Permissions = new[] { Developer }
                 },
                 new PermissionStereotype {
                     Name = "Editor",
